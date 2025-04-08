e95c3044cb703ab5f73b949368680c1047657b04
zippedalo <2845200679@qq.com>
2025-04-08 21:20:09 +0800

    Site updated: 2025-04-08 21:20:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/71211513.html","posts/34bcaab5.html"];function toRandomPost(){
+var posts=["posts/e8ec6fce.html","posts/71211513.html","posts/34bcaab5.html","posts/107c5971.html","posts/1b23d127.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
